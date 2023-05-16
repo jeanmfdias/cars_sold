c07ef1ac304939323dbfac25e5598f63776a7149
jeanmfdias <jeanmfdias@gmail.com>
2023-05-16 16:59:30 -0300

    Update edit screen to bootstrap

--- resources/views/cars/edit.blade.php
+++ resources/views/cars/edit.blade.php
@@ -6,29 +6,35 @@
         <form action="{{ route('cars.update', $car->id) }}" method="post">
             @method('put')
             @csrf
-            <div>
-                <label for="brand">Brand</label>
-                <input type="text" name="brand" value="{{ $car->brand }}">
+            <div class="mb-3">
+                <label for="brand" class="form-label">Brand</label>
+                <input type="text" name="brand" value="{{ $car->brand }}" class="form-control">
             </div>
-            <div>
-                <label for="model">Model</label>
-                <input type="text" name="model" value="{{ $car->model }}">
+            <div class="mb-3">
+                <label for="model" class="form-label">Model</label>
+                <input type="text" name="model" value="{{ $car->model }}" class="form-control">
             </div>
-            <div>
-                <label for="price">Price</label>
-                <input type="number" step="0.01" name="price" value="{{ $car->price }}">
+            <div class="mb-3">
+                <label for="price" class="form-label">Price</label>
+                <div class="input-group">
+                    <span class="input-group-text">R$</span>
+                    <input type="number" step="0.01" name="price" value="{{ $car->price }}" class="form-control">
+                </div>
             </div>
-            <div>
-                <label for="motor">Motor</label>
-                <input type="number" step="0.1" name="motor" value="{{ $car->motor }}">
+            <div class="mb-3">
+                <label for="motor" class="form-label">Motor</label>
+                <input type="number" step="0.1" name="motor" value="{{ $car->motor }}" class="form-control">
             </div>
-            <div>
-                <label for="horse_power">Horse Power</label>
-                <input type="number" name="horse_power" value="{{ $car->horse_power }}">
+            <div class="mb-3">
+                <label for="horse_power" class="form-label">Horse Power</label>
+                <div class="input-group">
+                    <input type="number" name="horse_power" value="{{ $car->horse_power }}" class="form-control">
+                    <span class="input-group-text">cv</span>
+                </div>
             </div>
-            <div>
-                <label for="color">Color</label>
-                <select name="color">
+            <div class="mb-3">
+                <label for="color" class="form-label">Color</label>
+                <select name="color" class="form-control">
                     @foreach($colors as $color)
                         <option value="{{ $color }}" {{ $color === $car->color ? 'selected' : '' }}>
                             {{ ucfirst($color) }}
@@ -36,20 +42,27 @@
                     @endforeach
                 </select>
             </div>
-            <div>
+            <div class="mb-3">
                 <p>It car was sold?</p>
-                <label for="sold_price">Sold price</label>
-                <input type="number" step="0.01" name="sold_price" value="{{ $car->sold_price }}">
+                <label for="sold_price" class="form-label">Sold price</label>
+                <div class="input-group">
+                    <span class="input-group-text">R$</span>
+                    <input type="number"
+                           step="0.01"
+                           name="sold_price"
+                           value="{{ $car->sold_price }}"
+                           class="form-control">
+                </div>
                 @if ($car->sold_at)
                     <p>Car was sold at {{ $car->sold_at->format('d/m/Y H:i:s') }}</p>
                 @endif
             </div>
-            <div>
-                <button type="submit">Update</button>
+            <div class="mb-3">
+                <button class="btn btn-success" type="submit">Update</button>
             </div>
         </form>
         <p>
-            <a href="{{ route('cars.index') }}">Go Back</a>
+            <a class="btn btn-outline-secondary" href="{{ route('cars.index') }}">Go Back</a>
         </p>
     </section>
 @endsection
