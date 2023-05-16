f29a277b5f664041f04a064a5bd6b1d2b88cfc97
jeanmfdias <jeanmfdias@gmail.com>
2023-05-16 16:42:39 -0300

    Update show screen to bootstrap

--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -3,50 +3,50 @@
 @section('content')
     <section>
         <h1>Car #{{ $car->id }}</h1>
-        <div>
-            <label for="brand">Brand:</label>
-            <p>{{ $car->brand }}</p>
+        <div class="mb-3">
+            <label for="brand" class="form-label">Brand:</label>
+            <p class="form-control">{{ $car->brand }}</p>
         </div>
-        <div>
-            <label for="model">Model:</label>
-            <p>{{ $car->model }}</p>
+        <div class="mb-3">
+            <label for="model" class="form-label">Model:</label>
+            <p class="form-control">{{ $car->model }}</p>
         </div>
-        <div>
-            <label for="price">Price:</label>
-            <p>R$ {{ number_format($car->price, 2, ',', '.') }}</p>
+        <div class="mb-3">
+            <label for="price" class="form-label">Price:</label>
+            <p class="form-control">R$ {{ number_format($car->price, 2, ',', '.') }}</p>
         </div>
-        <div>
-            <label for="motor">Motor:</label>
-            <p>{{ number_format($car->motor, 1, '.', '') }}</p>
+        <div class="mb-3">
+            <label for="motor" class="form-label">Motor:</label>
+            <p class="form-control">{{ number_format($car->motor, 1, '.', '') }}</p>
         </div>
-        <div>
-            <label for="horse_power">Horse Power:</label>
-            <p>{{ $car->horse_power }}cv</p>
+        <div class="mb-3">
+            <label for="horse_power" class="form-label">Horse Power:</label>
+            <p class="form-control">{{ $car->horse_power }}cv</p>
         </div>
-        <div>
-            <label for="color">Color:</label>
-            <p>{{ ucfirst($car->color) }}</p>
+        <div class="mb-3">
+            <label for="color" class="form-label">Color:</label>
+            <p class="form-control">{{ ucfirst($car->color) }}</p>
         </div>
-        <div>
-            <label for="created_at">Created At:</label>
-            <p>{{ $car->created_at->format('d/m/Y H:i:s') }}</p>
+        <div class="mb-3">
+            <label for="created_at" class="form-label">Created At:</label>
+            <p class="form-control">{{ $car->created_at->format('d/m/Y H:i:s') }}</p>
         </div>
-        <div>
-            <label for="updated_at">Updated At:</label>
-            <p>{{ $car->updated_at->format('d/m/Y H:i:s') }}</p>
+        <div class="mb-3">
+            <label for="updated_at" class="form-label">Updated At:</label>
+            <p class="form-control">{{ $car->updated_at->format('d/m/Y H:i:s') }}</p>
         </div>
         @if ($car->isSold())
-            <div>
-                <label for="sold_at">Sold At:</label>
-                <p>{{ $car->sold_at->format('d/m/Y H:i:s') }}</p>
+            <div class="mb-3">
+                <label for="sold_at" class="form-label">Sold At:</label>
+                <p class="form-control">{{ $car->sold_at->format('d/m/Y H:i:s') }}</p>
             </div>
-            <div>
-                <label for="sold_price">Sold price:</label>
-                <p>R$ {{ number_format($car->sold_price, 2, '.', ',') }}</p>
+            <div class="mb-3">
+                <label for="sold_price" class="form-label">Sold price:</label>
+                <p class="form-control">R$ {{ number_format($car->sold_price, 2, '.', ',') }}</p>
             </div>
         @endif
         <div>
-            <a href="{{ route('cars.index') }}">Go back</a>
+            <a class="btn btn-outline-secondary" href="{{ route('cars.index') }}">Go back</a>
         </div>
     </section>
 @endsection
